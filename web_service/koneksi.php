6082a082cabb94d71a278ebdd00513fdcc854489
uswatun utami <uswahutami8@gmail.com>
2020-04-05 15:33:42 +0700

    restandroid

---
+++ web_service/koneksi.php
@@ -0,0 +1,7 @@
+<?php
+ define('HOST','localhost');
+ define('USER','id12789037_pkl');
+ define('PASS','ftihebat654');
+ define('DB','id12789037_webservice');
+
+ $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
